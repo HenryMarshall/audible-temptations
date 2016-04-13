eefa94c1fe58a83d988a4536fe84a25360e55792
hbaughman <hbaughman741@gmail.com>
2016-04-13 17:11:44 -0400

    Add dotfiles (woops)

---
+++ js/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import RaisedButton from 'material-ui/lib/raised-button';
+
+const Home = () => (
+  <div>
+    <h1>Audible Temptations</h1>
+    <span>Hold your next novel hostage at the gym</span>
+    <RaisedButton label="Primary">Login with Amazon</RaisedButton>
+  </div>
+)
+
+export default Home;
